012dd2b8777cd25eb531bb1c296f24c881a3f3aa
KENNETH <kenneth.mburu@student.moringaschool.com>
2023-07-17 16:27:32 +0300

    Update app.js
    Contact method with setters and getters

---
+++ static/js/app.js
@@ -0,0 +1,38 @@
+// Another methods to go on with addressBook
+
+class Contact {
+    constructor(firstName, lastName, phoneNumber, emailAddress) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._phoneNumber = phoneNumber;
+        this._emailAddress = emailAddress;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+    set phoneNumber(value) {
+        this._phoneNumber = value;
+    }
+
+    get emailAddress() {
+        return this._emailAddress;
+    }
+    set emailAddress(value) {
+        this._emailAddress = value;
+    }    
+}
