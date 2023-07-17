ebd65e0140136e0692d0a13ca10c4bffadfebd71
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-17 15:09:37 +0300

    getters and setters for contact

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,36 @@ class Contact {
         this._emailAddress = emailAddress
     }
 
+    get firstName() {
+        return this._firstName;
+    }
 
+    set firstName(value) {
+        this._firstName = value;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    set lastName(value) {
+        this._lastName = value;
+    }
+
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+
+    set phoneNumber(value) {
+        this._phoneNumber = value;
+    }
+
+    get emailAddress() {
+        return this._emailAddress;
+    }
+
+    set emailAddress(value) {
+        this._emailAddress = value;
+    }
 
 }
