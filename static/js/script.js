779fa2d23a7eeb43841e1a7a697982cf84fb5350
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-17 15:09:09 +0300

    name data in style of setters and getters

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 class Contact {
+    constructor(firstName, lastName, phoneNumber, emailAddress) {
+        this._firstName = firstName
+        this._lastName = lastName
+        this._phoneNumber = phoneNumber
+        this._emailAddress = emailAddress
+    }
+
 
 
 }
